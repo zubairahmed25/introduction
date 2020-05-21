9f9f61a2be134b28efa4fd1f00f0192eefa51e83
Zubair Ahmed <zahmed@p2019738.home>
2020-05-21 07:04:56 -0700

    fifth update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <h1>Zubair's first REACT deployment on S3 using CloudFront - FINAL</h1>
+      <h1>Zubair's first AWS React App using S3 and Cloudfront</h1>
+      <h2>COUNTER APP</h2>
+      <h3>You clicked {count} times!</h3>
+
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 }
+
 
 export default App; 
