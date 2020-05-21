fa08154030d1e1be5f773c934719d3d4d82f291a
Zubair Ahmed <zahmed@p2019738.home>
2020-05-21 06:21:37 -0700

    edit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Zubair's first REACT deployment</h1>
+      <h1>Zubair's first REACT deployment on S3 using CloudFront</h1>
     </div>
   );
 }
